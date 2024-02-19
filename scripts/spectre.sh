924cf22e1092a4b3e4a87e130e72198c79fe2365
Jared-Tomei <99999382+Jared-Tomei@users.noreply.github.com>
2024-02-19 14:17:26 -0600

    Add files via upload

---
+++ scripts/spectre.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+##spectre needs mosdepth so run that 1st
+##from general sample level
+mkdir cnv
+cd cnv/
+mkdir mosdepth
+cd mosdepth/
+##need to activate the mosdepth conda enviroment
+conda activate mosdpeth
+mosdepth -n -t 4 --fastmode --by 1000 samplename_mosdepth sample.sorted.bam
+conda deactivate
+##Run spectre tool
+##If rerunning sample makesure to delete spectre output file or it will crash
+##match bin sizes between mosdepth and spectre but that number can be changed
+conda activate spectre
+spectre.py CNVCaller \ --bin-size 1000 \ --coverage mosdepth/ \ --sample-id sampleid \ --output-dir Spectre_results/ \ --reference /data/ref/NCBI/GRCh38/Homo_sapiens_assembly38_noALT_noHLA_noDecoy_ERCC.fasta
